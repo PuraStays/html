e03bb0983f797deb9879c17b4de7e3ac83e18526
ranjan <kumar.ranjan89@gmail.com>
2017-08-12 01:00:41 +0530

    deleted booking1 and booking2 unnecessary folder

--- booking1/book.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../includes/db.inc.php");
-$id = 215;
-$db = new DB();
-$qry = "select * from hotelogix_details where Id = '$id'";
-$result = $db->_query($qry);
-$row = mysqli_fetch_array($result);
-var_dump($row);
-?>
--- booking1/pay/js/init.js
+++
@@ -1,9 +0,0 @@
-document.createElement('section');
-document.createElement('header');
-document.createElement('details');
-document.createElement('footer');
-$(document).ready(function(e) {
-    var a = $('.msg').val();
-	var b = $.trim(a);
-	$('.msg').val(b);
-});
