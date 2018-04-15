ccac5dc4cd17ac8649fc1f694269405c04106035
ranjan <kumar.ranjan89@gmail.com>
2018-04-15 10:39:49 +0530

    adding undermaintenance page

---
+++ undermaintenance.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page not found | 404</title>
+    <link rel="shortcut icon" href="../images/favicon.ico">
+    <!-- Bootstrap -->
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap-social.css">
+    <link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/jquery.bxslider.css" rel="stylesheet">
+
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@purastays">
+    <meta name="twitter:title" content="<Page not found | 404">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:creator" content="@purastays">
+    <meta name="twitter:image:src" content="http://www.purastays.com/images/pura-stays-logo.png">
+    <meta name="twitter:url" content="http://www.purastays.com/">
+
+    <!-- Open Graph Card data -->
+    <meta property="og:title" content="Page not found | 404" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://www.purastays.com/" />
+    <meta property="og:image" content="http://www.purastays.com/images/pura-stays-logo.png" />
+    <meta property="og:description" content="" />
+    <meta property="og:site_name" content="Pura Stays" />
+    <meta property="fb:admins" content="1125962897414339" />
+    <meta property="fb:app_id" content="1152657561422465" />
+    
+    <?php include_once("includes/taghead.php") ?>
+  </head>
+  <body>
+  <?php include_once("includes/tagbody.php") ?>
+    <header>
+        <!-- Login Modal -->
+        <?php include_once("includes/login-modal.php");?>
+        <section class="pura-banner-inner">
+            <div class="top-shadow"></div>
+            
+            
+            <div class="bann-txt-container">
+                <div class="container">
+                    <h1></h1>
+                    <h3>Undermaintenance</h3>
+                </div>
+            </div>
+            <div class="pura-slider">
+                <img src="images/banner/Error404.jpg" alt="404 Page not found">
+            </div>
+        </section>
+    </header>    
+    
+    <section class="sec contact">
+        <div class="container">
+            <div class="other-form">
+		<h3>This page is undermaintenance, please visit after sometime.</h3>
+            </div>                                    
+        </div>                  
+    </section>
+    
+    
+ 
+  </body>
+</html>
