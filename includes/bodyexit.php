0e9341075a00a4ecaa1a05a5633d9b1ab5175df4
ranjan <kumar.ranjan89@gmail.com>
2018-05-03 08:57:17 +0530

    adding request a callback

--- includes/bodyexit.php
+++ includes/bodyexit.php
@@ -1,3 +1,74 @@
+<div id="chooseResort" class="modal animated fade resort selection" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+    <!-- Modal content-->
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title">Select a Holiday Stay</h4>
+        </div>
+        <div class="modal-body">
+            <div class="resort-selection-modal">
+                <select></select>
+                <button type="button" class="btn btn-pura">Book</button>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+<div id="reqCallBack" class="modal animated fade resort selection request-a-callback-dialog" role="dialog">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+    <!-- Modal content-->
+        <div class="modal-body">
+            
+            <div class="request-callback-content">
+                <div class="callback-img-container"><img src="/images/request-a-callback.jpg" alt="purastays" /></div>
+                <div class="callback-content-container">
+                    <a class="close" data-dismiss="modal" aria-hidden="true">&times;</a>
+                    <h3>Request a callback</h3>
+                    <h5>Fill the form below, we will revert you within 24 hours.</h5>
+                    <div class="status-message"></div>
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Your Name</label>
+                            <input type="text" class="form-control" id="name" placeholder="Please enter youe name here" name="name">
+                            
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" id="email" placeholder="Please enter your email id here" name="email">
+                            
+                        </div>
+
+                        <div class="form-group">
+                            <label for="mobile">Mobile Number</label>
+                            <input type="text" class="form-control" id="mobile" placeholder="Please enter your 10 digit mobile number" name="mobile">
+                            
+                        </div>
+                        <input type="submit" class="btn btn-pura" value="send">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+<style>
+    .resort-selection-modal{
+        padding: 30px 0;
+        display: flex;
+    }
+    .resort-selection-modal select{
+        flex: 1;
+        width: 100%;
+        margin-right: 10px;
+        outline: none;
+        border-radius: 0 !important;
+    }
+
+</style>
 <script>
     $(document).ready(function(){
         var resultObject, bookLink;
@@ -107,39 +178,80 @@
                 });
             })
         }
+
+        $('#req_callback').on('click', function() {
+            $('#reqCallBack').modal({
+                show: 'false'
+            });
+        })
     })
+
+    var pura = (function() {
+        var component = {
+            "requestCallBack": jQuery('#reqCallBack'),
+            "endpoint": "http://admin.purastays.com:3000/api/web/request_callback"
+        }
+        
+        function reset() {
+            var msg = component.requestCallBack.find('.status-message');
+            if(msg.text()) {
+                if(msg.hasClass('error'))
+                    msg.removeClass('error');
+                if(msg.hasClass('success'))
+                    msg.removeClass('success');
+
+                msg.text("");
+            }
+        }
+
+        function formatData(data) {
+            var updatedData = {};
+            data.forEach(function(element) {
+              updatedData[element.name] = element.value;
+            });
+            return updatedData;
+        }
+
+        function handleRequestCallback() {
+            var form = component.requestCallBack.find('form');
+            var btn = component.requestCallBack.find(".btn");
+            var errorDiv = '<small class="form-text text-danger error"></small>'; 
+            
+            form.on('submit', function(e) {
+                if(true) {
+                    e.preventDefault(); 
+                    var data = form.serializeArray();                  
+                    $.ajax({
+                      type: 'POST',
+                      url: component.endpoint,
+                      crossDomain: true,
+                      data: JSON.stringify(formatData(data)),
+                      dataType: "json",
+                      headers: { 
+                        "Content-type" : "application/json"
+                      },
+                      success: function(resultData) { 
+                        if(resultData.status) {                            
+                            component.requestCallBack.find('input[type=text], input[type=email]').val("");
+                            component.requestCallBack.find('.status-message').addClass('success').html(resultData.message);
+                            setTimeout(function() {
+                                $('#reqCallBack').modal('hide');
+                                reset();
+                            },1500)                        
+                        }                        
+                      }
+                    });
+
+                }                
+            })            
+        }
+
+        function init() {
+            handleRequestCallback();
+        }
+
+        init();
+
+    })();
     
 </script>
-
-<div id="chooseResort" class="modal animated fade resort selection" role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
-    <!-- Modal content-->
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title">Select a Holiday Stay</h4>
-        </div>
-        <div class="modal-body">
-            <div class="resort-selection-modal">
-                <select></select>
-                <button type="button" class="btn btn-pura">Book</button>
-            </div>
-        </div>
-    </div>
-
-  </div>
-</div>
-<style>
-    .resort-selection-modal{
-        padding: 30px 0;
-        display: flex;
-    }
-    .resort-selection-modal select{
-        flex: 1;
-        width: 100%;
-        margin-right: 10px;
-        outline: none;
-        border-radius: 0 !important;
-    }
-
-</style>
